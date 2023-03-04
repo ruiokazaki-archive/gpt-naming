b44b1f7315a0c5317260dbc7049be1db5f6ffe94
RuiOkazaki <okazaki200226@gmail.com>
2023-03-04 19:26:21 +0900

    feat: ask name

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/AlecAivazis/survey/v2"
+)
+
+var questions = []*survey.Question{
+	{
+		Name: "name",
+		Prompt: &survey.Input{
+			Message: "What is your name?",
+		},
+		Validate: survey.Required,
+	},
+}
+
 func main() {
+	answers := struct {
+		Name string
+	}{}
+
 	println("Hello, World!")
+
+	err := survey.Ask(questions, &answers)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Printf("Hello, %s!", answers.Name)
 }
